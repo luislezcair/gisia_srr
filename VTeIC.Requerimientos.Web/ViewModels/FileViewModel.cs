aabe797756201e968bd8148b371bc163e0f781b8
Luis Lezcano Airaldi <luislezcair@gmail.com>
2016-07-05 20:42:03 -0300

    Mostrar URL en la tabla de archivos

--- VTeIC.Requerimientos.Web/ViewModels/FileViewModel.cs
+++ VTeIC.Requerimientos.Web/ViewModels/FileViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.Diagnostics;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 
 namespace VTeIC.Requerimientos.Web.ViewModels
@@ -10,27 +13,68 @@ namespace VTeIC.Requerimientos.Web.ViewModels
         public FileViewModel(string path)
         {
             FullPath = path;
+            LoadMetadata();
         }
 
         public string FileName
         {
             get
             {
-                //remove the file extension for the name
+                // Remove the file extension for the name
                 return Path.GetFileNameWithoutExtension(FullPath);
             }
         }
 
         public string VirtualPath { get; set; }
 
-        public string SourceURL
+        public string SourceURL { get; private set; }
+
+        private void LoadMetadata()
+        {
+            string file = Path.ChangeExtension(FullPath, "json");
+
+            if(File.Exists(file))
+            {
+                using (StreamReader r = new StreamReader(file))
+                {
+                    string jsonString = r.ReadToEnd();
+                    var metadata = JsonConvert.DeserializeObject<MetadataFile>(jsonString);
+                    SourceURL = metadata.Metadata.url;
+                }
+            }
+        }
+    }
+
+    public class MetadataFile
+    {
+        public List<MetadataDocument> document { get; set; }
+
+        public MetadataDocument Metadata
         {
             get
             {
-                Debug.Print(FullPath);
-                string file = Path.GetFileNameWithoutExtension(VirtualPath) + ".json";
-                return file;
+                return document[0];
             }
+        }
+    }
+
+    public class MetadataDocument
+    {
+      // "url": "https://www.youtube.com/", 
+      // "id_request": "12", 
+      // "weight": 1.0276595799263404, 
+      // "filename": "01_www.youtube.com.html"
+        public string url { get; set; }
+    }
+
+    public static class StringExt
+    {
+        // Limita la longitud de la cadena en maxLength y agrega "..." si la cortó
+        public static string Truncate(this string value, int maxLength)
+        {
+
+            if (string.IsNullOrEmpty(value)) { return value; }
+            return value.Length <= maxLength ? value : (value.Substring(0, maxLength) + "..."); 
         }
     }
 }
